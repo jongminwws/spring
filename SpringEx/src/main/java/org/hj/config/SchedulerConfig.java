7a26b72264abe77ceb9c217f23bfb9f0080e0835
jongminwws <wkdwhd43@naver.com>
2024-07-01 17:21:45 +0900

    spring 연습

---
+++ SpringEx/src/main/java/org/hj/config/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package org.hj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+    // 추가 설정이 필요한 경우 여기에 작성합니다.
+} 
